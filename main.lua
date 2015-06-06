7e642cf82bd66e0ec6af288a19407afbb63c8c6a
Juan Manuel González <juan0511@martina.local>
2015-06-06 19:01:31 -0300

    agrego sonidos, musica, vidas. saco factorial de main que no se usaba, el tema de ganar o peder con toda su logica. limito movimiento del jugador a la pantalla.

--- main.lua
+++ main.lua
@@ -3,14 +3,28 @@
 
 function love.load()
 
-	game = {} -- new table for game data
+	-- new table for game data
+	game = {} 
 	game.state = "mainMenu"
-	game.won = false
 	game.score = 0
 	game.highScores = {[1]={name="AAA", score=0, date=os.date("%c")}, [2]={name="BBB", score=0, date=os.date("%c")}, [3]={name="CCC", score=0, date=os.date("%c")}, [4]={name="DDD", score=0, date=os.date("%c")}, [5]={name="EEE", score=0, date=os.date("%c")}}
 	
-	bg = love.graphics.newImage("bg.png")
+	-- load external graphics
+	bgGame = love.graphics.newImage("bg.png")
 	
+	-- loadSFX
+	startSound = love.audio.newSource("sounds/start.wav", "static")
+	shotSound = love.audio.newSource("sounds/laser.wav", "static")
+	explosionSound = love.audio.newSource("sounds/explosion.wav", "static")
+	endSound = love.audio.newSource("sounds/end.wav", "static")
+	
+	-- loadMusic
+	menuMusic = love.audio.newSource("music/menuMusic.wav", "static")
+	menuMusic:setLooping(true)
+	gameMusic = love.audio.newSource("music/gameMusic.wav", "static")
+	gameMusic:setLooping(true)
+
+	menuMusic:play()	
 	initGame()
 end
 
@@ -22,110 +36,114 @@ function love.update(dt)
 	else
 
 		if game.state == "playGame" then
-			if (#enemies == 0) then
-				-- game action
-				-- no bricks left
-				-- you win!!!
-				game.state = "gameOver"
-				game.won = true
-				table.insert(game.highScores, {name="FFF", score=game.score, date=os.date("%c")})
-				table.sort(game.highScores, compareScoresGt)
-				
+			-- game action
+			-- keyboard actions for player
+			if ((love.keyboard.isDown("up")) and (hero.y > 0)) then
+				hero.y = hero.y - hero.speed*dt
 			else
-				-- game action
-				-- bricks left
-				-- keyboard actions for our hero
-				if love.keyboard.isDown("up") then
-					hero.y = hero.y - hero.speed*dt
-				elseif love.keyboard.isDown("down") then
+				if ((love.keyboard.isDown("down")) and (hero.y < (600-hero.height))) then
 					hero.y = hero.y + hero.speed*dt
 				end
+			end
 
-				local remEnemy = {}
-				local remShot = {}
+			local remEnemy = {}
+			local remShot = {}
 
-				-- update the shots
-				for i,v in ipairs(hero.shots) do
+			-- update the shots
+			for i,v in ipairs(hero.shots) do
 
-					-- move them right right right
-					v.x = v.x + dt * 100
+				-- move them right right right
+				v.x = v.x + dt * 100
 
-					-- mark shots that are not visible for removal
-					if v.x > 800 then
+				-- mark shots that are not visible for removal
+				if v.x > 800 then
+					table.insert(remShot, i)
+				end
+				
+				-- check for collision with enemies
+				for ii,vv in ipairs(enemies) do
+					if CheckCollision(v.x,v.y,5,2,vv.x,vv.y,vv.width,vv.height) then
+
+						-- mark that enemy for removal
+						table.insert(remEnemy, ii)
+						-- mark the shot to be removed
 						table.insert(remShot, i)
-					end
-				
-					-- check for collision with enemies
-					for ii,vv in ipairs(enemies) do
-						if CheckCollision(v.x,v.y,5,2,vv.x,vv.y,vv.width,vv.height) then
-
-							-- mark that enemy for removal
-							table.insert(remEnemy, ii)
-							-- mark the shot to be removed
-							table.insert(remShot, i)
+							
+						local sfx = explosionSound:clone()
+						sfx:play()
 						
-							game.score = game.score + 100;
-						end
+						game.score = game.score + 100;
 					end
 				end
+			end
 
-				-- remove the marked enemies
-				for i,v in ipairs(remEnemy) do
-					table.remove(enemies, v)
-				end
+			-- remove the marked enemies
+			for i,v in ipairs(remEnemy) do
+				table.remove(enemies, v)
+			end
 
-				for i,v in ipairs(remShot) do
-					table.remove(hero.shots, v)
-				end
+			for i,v in ipairs(remShot) do
+				table.remove(hero.shots, v)
+			end
 				
+			-- detect the farther position
+			local farthest = 0
+			for i,v in ipairs(enemies) do
+				if (v.x > farthest) then
+					farthest = v.x
+				end
+			end
 				
-				-- detect the farther position
-				local farthest = 0
-				for i,v in ipairs(enemies) do
-					if (v.x > farthest) then
-						farthest = v.x
-					end
+			--generate more enemies
+			if farthest < 700 then
+				local newSize = math.random(2,5)
+				local isCrazy = true
+				if (math.random(0,1)==0) then
+					isCrazy = false
 				end
-				--generate more enemies
-				if farthest < 700 then
-					local newSize = math.random(2,5)
-					local isCrazy = true
-					if (math.random(0,1)==0) then
-						isCrazy = false
-					end
-					for j=0,newSize do
-						generateEnemy(newSize, j, isCrazy, not phase)
-					end
+				for j=0,newSize do
+					generateEnemy(newSize, j, isCrazy, not phase)
 				end
-				local amplitud = 10
-				local angular = (2*math.pi / 20) --2*PI / T
-				-- update those evil enemies
-				for i,v in ipairs(enemies) do
-					-- let them fall down slowly
-					--v.x = v.x - dt * 25
-					v.x = v.x - dt * 30
-					if (v.isCrazy) then
-						--v.y = v.y - (amplitud * math.sin( angular * math.abs(v.x-800)))
-						if v.phase == false then
-							v.y = v.y - (amplitud * math.sin( angular * math.abs(v.x-800)))
-							phase = false
-						else 
-							v.y = v.y - (amplitud * math.cos( angular * math.abs(v.x-800)))
-							phase = true
-						end
-					end
+			end
+			local amplitud = 10
+			local angular = (2*math.pi / 20) --2*PI / T
 
-					-- check for collision with ground
-					if v.x < 10 then
-						-- you lose!!!
+			-- update those evil enemies
+			for i,v in ipairs(enemies) do
+				-- let them fall down slowly
+				--v.x = v.x - dt * 25
+				v.x = v.x - dt * 30
+				if (v.isCrazy) then
+					--v.y = v.y - (amplitud * math.sin( angular * math.abs(v.x-800)))
+					if v.phase == false then
+						v.y = v.y - (amplitud * math.sin( angular * math.abs(v.x-800)))
+						phase = false
+					else 
+						v.y = v.y - (amplitud * math.cos( angular * math.abs(v.x-800)))
+						phase = true
+					end
+				end
+
+				-- check for collision with left border
+				if v.x < 10 then
+					-- you lose a life!
+					table.remove(enemies, i)
+					hero.livesLeft = hero.livesLeft - 1
+					local sfx = endSound:clone()
+					sfx:play()
+					
+					if hero.livesLeft < 0 then
 						game.state = "gameOver"
 					end
 				end
+			end
 				
-				if game.state == "gameOver" then
-					table.insert(game.highScores, {name="FFF", score=game.score, date=os.date("%c")})
-					table.sort(game.highScores, compareScoresGt)
-				end
+			if game.state == "gameOver" then
+				-- you lose the game!
+				table.insert(game.highScores, {name="FFF", score=game.score, date=os.date("%c")})
+				table.sort(game.highScores, compareScoresGt)
+				gameMusic:stop()
+				menuMusic:play()
 			end
 		else
 			-- game over!
@@ -145,7 +163,7 @@ function love.draw()
 			love.graphics.reset()
 			-- let's draw a background
 			love.graphics.setColor(255,255,255,255)
-			love.graphics.draw(bg)
+			love.graphics.draw(bgGame)
 
 			-- let's draw some ground
 			love.graphics.setColor(0,255,0,255)
@@ -166,18 +184,21 @@ function love.draw()
 			for i,v in ipairs(enemies) do
 				love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
 			end
+			
+			-- status messages at bottom of screen
 			love.graphics.setColor(255,255,255,255)
 			love.graphics.print("Tu puntaje es: " .. game.score .. ".", 600, 550)
+			love.graphics.print("Te quedan " .. hero.livesLeft .. " vidas:", 50, 550)
+			for i=0,hero.livesLeft-1 do
+				love.graphics.setColor(255,255,0,255)
+				love.graphics.rectangle("fill", 175 + i*(hero.width+10), 542, hero.width, hero.height)
+			end	
 		else
 			-- game over!
 			love.graphics.reset()
 			love.graphics.setBackgroundColor(0, 0, 0)
 			love.graphics.print("Fin.", 400, 180)
-			if game.won == true then
-				love.graphics.print("GANASTE!", 400, 200)
-			else
-				love.graphics.print("Perdiste el juego :(", 400, 200)
-			end
+			love.graphics.print("Perdiste el juego :(", 400, 200)
 			love.graphics.print("Presioná SPACE para jugar de nuevo o ESC para salir!", 400, 220)
 			love.graphics.print("Tu puntaje es: " .. game.score .. ".", 400, 240)
 			love.graphics.print("Tabla de Puntajes", 400, 300)
@@ -200,6 +221,9 @@ function love.keyreleased(key)
 		-- main menu
 		if (key == " ") then
 			game.state = "playGame"
+			menuMusic:stop()
+			startSound:play()
+			gameMusic:play()
 		end
 	else
 		if game.state == "playGame" then
@@ -214,6 +238,9 @@ function love.keyreleased(key)
 				game.state = "playGame"
 				hero = nil
 				enemies = nil
+				menuMusic:stop()
+				startSound:play()
+				gameMusic:play()
 				initGame()
 			end
 		end
@@ -232,6 +259,7 @@ function initGame()
 	hero.width = 15
 	hero.height = 30
 	hero.speed = 150
+	hero.livesLeft = 3
 	hero.shots = {} -- holds our fired shots
 
 	local size = 8
@@ -245,8 +273,6 @@ function generateEnemy(size, i, crazy, ph)
 	enemy = {}
 	enemy.width = 20
 	enemy.height = 40
-	--enemy.x = i * (enemy.width + 60) + 100
-	--enemy.y = i * (enemy.height + 40) + 50
 	local aux = 600/math.pow(2,size+1)
 	local beforeAux = 600/(size+1)
 	enemy.y =  aux + beforeAux * i
@@ -256,17 +282,13 @@ function generateEnemy(size, i, crazy, ph)
 	table.insert(enemies, enemy)
 end
 
-function compareScoresGt(w1,w2)
-	if w1.score > w2.score then
-		return true
-	end
-end
-
 function shoot()
 	local shot = {}
 	shot.x = hero.x + hero.width
 	shot.y = hero.y + hero.height/2
-
+	local sfx = shotSound:clone()
+	sfx:play()
+	
 	table.insert(hero.shots, shot)
 end
 
@@ -281,11 +303,8 @@ function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
 		y2 < y1+h1
 end
 
--- defines a factorial function
-function fact (n)
-  if n == 0 then
-    return 1
-  else
-    return n * fact(n-1)
-  end
+function compareScoresGt(w1,w2)
+	if w1.score > w2.score then
+		return true
+	end
 end
